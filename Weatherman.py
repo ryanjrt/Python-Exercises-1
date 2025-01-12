575176b83235c5e1c1e45ad7413d14c849e7e4b5
ryanjrt <tham.jurong@gmail.com>
2025-01-12 05:00:12 +0000

    Added functionality for multiple cities

--- Weatherman.py
+++ Weatherman.py
@@ -1,30 +1,35 @@
 import requests
 
 
+cities = input("Please enter your cities of choice (separated by commas ','): ").split(",")
+cities = [city.strip() for city in cities]
 
-
-city = input("Please enter your city of choice: ")#.strip()
-
+print(cities)
 #API endpoint
 base_url = "http://api.weatherapi.com/v1"
 selected_api = "/current.json"
 api_key = "?key=e3b8fde100954d0ea0d165332251001"
 
-api_call = base_url + selected_api + api_key + "&q=" + city
+api_call = []
+
+for city in cities:
+    call = base_url + selected_api + api_key + "&q=" + city
+    api_call.append(call)
+    
 print(api_call)
 
-response = requests.get(api_call)
+for call in api_call:
+    response = requests.get(call)
+    #Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        print(f" City: {data['location']['name']}")
+        print(f" Country: {data['location']['country']}")
+        print(f" Temperature: {data['current']['temp_c']} C")
+        print(f" Weather Conditions: {data['current']['condition']['text']}")
+        print(f" Humidity: {data['current']['humidity']}")
+        print(f" Last Updated: {data['current']['last_updated']}\n")
 
-#Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the JSON response
-    data = response.json()
-    print(f" City: {data['location']['name']}")
-    print(f" Country: {data['location']['country']}")
-    print(f" Temperature: {data['current']['temp_c']} C")
-    print(f" Weather Conditions: {data['current']['condition']['text']}")
-    print(f" Humidity: {data['current']['humidity']}")
-    print(f" Last Updated: {data['current']['last_updated']}")
-
-else:
-    print(f"Failed to retrieve data. Status code: {response.status_code}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
